18935d6972382e8d18d0adf839bf0fa77b144d8f
nidhii-kumari <nidhi_kumari2@epam.com>
2026-04-21 07:54:10 +0000

    Fix formatting in activities dictionary for consistency

--- src/app.py
+++ src/app.py
@@ -38,7 +38,8 @@ activities = {
         "schedule": "Mondays, Wednesdays, Fridays, 2:00 PM - 3:00 PM",
         "max_participants": 30,
         "participants": ["john@mergington.edu", "olivia@mergington.edu"]
-    }
+    },
+    
 }
 
 
